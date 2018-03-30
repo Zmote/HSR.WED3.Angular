051be24d83d1f727a32a2fe256e541a54d47c1cd
Zafer Dogan <zdogan@hsr.ch>
2018-03-31 01:55:30 +0200

    quickfix for deploy caused by empty directories in dashboard

--- src/app/dashboard/index.ts
+++ src/app/dashboard/index.ts
@@ -1,7 +1,8 @@
 export * from './components';
-export * from './models';
-export * from './resources';
-export * from './services';
+// Uncomment these when appropriate classes exist, empty folder cause build to fail on deploy
+// export * from './models';
+// export * from './resources';
+// export * from './services';
 
 // TODO: Add barrel exports of your features (export * from '...';) here...
 
