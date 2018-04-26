6be30662b27668de8c4f65f1904004b0d7078b65
Elias Brunner <elias@Eliass-MacBook-Pro.local>
2018-04-26 10:35:31 +0200

    guards merged successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,6 @@ import {NavComponent} from './nav/nav.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule} from '@angular/material';
 import {MatDividerModule} from '@angular/material/divider';
-import {DashboardModule} from './dashboard/dashboard.module';
-import {DashboardGuard} from './auth/services/dashboard-guard.service';
 
 @NgModule({
   declarations: [
